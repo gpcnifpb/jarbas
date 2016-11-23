702f1cf7df216175b194dd4de9c5d222f7ec5e7f
Neudson Jose <neudson@debian>
2016-11-23 09:18:38 -0300

    alterando runAtacado e runMonitorado para instalar o jarbas nos servidores

--- Project/functions.sh
+++ Project/functions.sh
@@ -90,11 +90,15 @@ function runSemAtaque() {
   echo "RODADA $1 SEM ATAQUE"
 
 	echo "runAtacado"
-  sshpass -p 'vagrant' ssh root@192.168.0.200 'apt-get -y install git && cd /gpcn/atacado/scripts/ && git clone https://github.com/gpcnifpb/jarbas.git'
+  if [[ $numRodada -eq 1 ]]; then
+    sshpass -p 'vagrant' ssh root@192.168.0.200 'apt-get -y install git && cd /gpcn/atacado/scripts/ && git clone https://github.com/gpcnifpb/jarbas.git'
+  fi
   sshpass -p 'vagrant' ssh root@192.168.0.200 'bash /gpcn/atacado/scripts/jarbas run atacado '$numRodada $tipoDeExperimento &
 
   echo "runMonitorado"
-  sshpass -p 'vagrant' ssh root@192.168.10.201 'apt-get -y install git && cd /gpcn/monitorado/scripts/ && git clone https://github.com/gpcnifpb/jarbas.git'
+  if [[ $numRodada -eq 1 ]]; then
+    sshpass -p 'vagrant' ssh root@192.168.10.201 'apt-get -y install git && cd /gpcn/monitorado/scripts/ && git clone https://github.com/gpcnifpb/jarbas.git'
+  fi
   sshpass -p 'vagrant' ssh root@192.168.10.201 'bash /gpcn/monitorado/scripts/jarbas run monitorado' $numRodada $tipoDeExperimento &
   for i in `seq 1 6`
   do
